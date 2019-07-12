eacebd22997eea2525090084defd8bd45ee145fd
Maram <smaram7@gatech.edu>
2019-07-12 12:00:02 -0400

    plot

--- sign_plotter.py
+++ sign_plotter.py
@@ -13,37 +13,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import style
 import pandas as pd 
+import matplotlib.cm as cm
+import matplotlib.colors as colors
 
-style.use('ggplot')
+
+style.use('fivethirtyeight')
 
 
 #load data frame of sign 
-df_sign = pd.read_csv('signs.csv')
+df1 = pd.read_csv('signs_4.csv',name=['SignId','pX','pY','Retro'])
 #create df with only required 
-df1 = df_sign[['pX','pY','Retro']]
+#df1 = df_sign[['SignId','pX','pY','Retro']]
 print(df1.head())
 
-x3=[i for i in df1['pX']]
-y3=[i for i in df1['pY']]
-z3=[i for i in df1['Retro']]
+x3=[i for i in df1['pX'] ]
+y3=[i for i in df1['pY'] ]
+z3=0
+
+dx=0.05 *np.ones(len(x3))
+dy=0.05
+dz=[i for i in df1['Retro']]
 
 
+fig = plt.figure()
+ax1 = fig.add_subplot(111, projection='3d')
 
-# fig = plt.figure()
-# ax1 = fig.add_subplot(111, projection='3d')
+max_dz=max(dz)
 
-# x3 = [1,2,3,4,5,6,7,8,9,10]
-# y3 = [5,6,7,8,2,5,6,3,7,2]
-# z3 = np.zeros(10)
-# dx = np.ones(10)
-# dy = np.ones(10)
-# dz = [1,2,3,4,5,6,7,8,9,10]
+normalized_dz=[i/max_dz for i in dz]
 
-# ax1.bar3d(x3, y3, z3, dx, dy, dz)
+colors = plt.cm.jet(dz)
+print(type(colors))
+#ax1.bar3d(x3,y3,z3)
+ax1.bar3d(x3, y3, z3, dx, dy, dz,color=colors)
 
 
-# ax1.set_xlabel('x')
-# ax1.set_ylabel('y')
-# ax1.set_zlabel('retro')
-
-# plt.show()
+ax1.set_xlabel('x')
+ax1.set_ylabel('y')
+ax1.set_zlabel('retro')
+ax1.set_zlim3d(0,1)
+plt.show()
