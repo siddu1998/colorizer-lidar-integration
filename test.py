635963cadbed6b6a5c64004d53379511cbf56f2e
Sai Siddartha Maram <smaram_be16@thapar.edu>
2019-07-13 18:53:55 -0400

    tkinter pop up

--- test.py
+++ test.py
@@ -5,8 +5,17 @@ from matplotlib import style
 import pandas as pd 
 import matplotlib.cm as cm
 import matplotlib.colors as colors
+import matplotlib
+from matplotlib.figure import Figure
+
+
+
+
 
 matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg,NavigationToolbar2Tk
+
+
 
 import tkinter as tk
 from tkinter import ttk 
@@ -74,6 +83,103 @@ class PageTwo(tk.Frame):
         button2=ttk.Button(self,text="Bad Points", 
         command=lambda: controller.show_frame(PageOne))
         button2.pack()
+
+        #load data frame of sign 
+        df1 = pd.read_csv('signs_13.csv')
+        #create df with only required 
+        df_sign = df1[['SignId','pX','pY','Retro']]
+
+        #less then 0.3
+        x3_very_poor=[]
+        y3_very_poor=[]
+        dz_very_poor=[]
+
+        #between 0.31-0.5
+        x3_average_point=[]
+        y3_average_point=[]
+        dz_average_point=[]
+
+        #between 0.51-0.6
+        x3_above_average_point=[]
+        y3_above_average_point=[]
+        dz_above_average_point=[]
+
+        #between 0.61-1
+        x3_great_point=[]
+        y3_great_point=[]
+        dz_great_point=[]
+
+
+        for i,row in df_sign.iterrows():
+            if row['Retro']<0.3:
+                x3_very_poor.add(row['pX'])
+                y3_very_poor.add(row['pY'])
+                dz_very_poor.add(row['Retro'])
+            if 0.31<row['Retro']<0.5:
+                x3_average_point.append(row['pX'])
+                y3_average_point.append(row['pY'])
+                dz_average_points.append(row['Retro'])
+            if 0.51<row['Retro']<0.6:
+                x3_above_average_point.append(row['pX'])
+                y3_above_average_point.append(row['pY'])
+                dz_above_average_point.append(row['Retro'])
+            if row['Retro']>0.61:
+                x3_great_point.append(row['pX'])
+                y3_great_point.append(row['pY'])
+                dz_great_point.append(row['Retro'])
+            
+
+        #length and width of each bar
+        dx=0.03
+        dy=0.03
+
+        #heights of each point category
+        z3_very_poor=np.zeros(len(x3_very_poor))
+        z3_average_point=np.zeros(len(x3_average_point))
+        z3_above_average=np.zeros(len(x3_above_average_point))
+        z3_great_point=np.zeros(len(x3_great_point))
+
+        fig = Figure(figsize=(2,2), dpi=75)
+        canvas=FigureCanvasTkAgg(fig,self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+
+
+        ax1 = fig.add_subplot(111, projection='3d')
+
+        if len(x3_great_point)>0:
+            #color the point green
+            ax1.bar3d(x3_great_point, y3_great_point,z3_great_point, dx, dy, dz_great_point,color=(0,1,0,0.6))
+        else:
+            pass
+        if len(x3_average_point)>0:
+            #color the point y
+            ax1.bar3d(x3_average_point, y3_average_point,z3_average_point, dx, dy, dz_average_point,color='y')
+        else:
+            pass
+        if len(x3_very_poor)>0:
+            #color the point red
+            ax1.bar3d(x3_very_poor, y3_very_poor, z3_very_poor, dx, dy, dz_very_poor,color='r')
+        else:
+            pass
+
+        if len(x3_above_average_point)>0:
+            #color the point orange
+            ax1.bar3d(x3_above_average_point, y3_above_average_point, z3_above_average, dx, dy,dz_above_average_point ,color=(0.6,0.3,0.0,0.6))
+        else:
+            pass
+
+
+
+        ax1.set_xlabel('x')
+        ax1.set_ylabel('y')
+        ax1.set_zlabel('retro')
+        ax1.set_zlim3d(0,1)
+
 
                 
 app=SignAnalyzer()
